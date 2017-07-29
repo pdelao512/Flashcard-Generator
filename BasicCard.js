e1af9c1845b3bdf922e79312beac07d1012d8779
pdelao512 <pdelao512@gmail.com>
2017-07-28 22:45:44 -0500

    add js.

--- BasicCard.js
+++ BasicCard.js
@@ -1,1 +1,11 @@
+var BasicCard = function(front, back){
+//this.card will hold all our card objects
+	this.card = [];
+
+	this.addCard = function(f, b){
+		this.front.push(new Card(f,b));
+	};
+};
+
+//Exports the Basic flash card constructor.
 module.exports = BasicCard;
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,1 +1,16 @@
+
+var ClozeCard = function(text, cloze){
+	var text = text;
+	var cloze = cloze;
+
+	if (!textc.includes(clozeC)){
+		console.log("Error: The deletion does not appear in your text");
+		return;
+	}
+
+	this.text = text;
+	this.cloze = cloze;
+	this.clozeTest = text.replace(cloze, "________");
+};
+
 module.exports = ClozeCard;
